82d66ef568a19f60930465db8c2c4ad73e240d77
Roel Stevens <rshunter99@gmail.com>
2021-07-05 20:45:21 +0200

    Added tray icon

--- ScreenDimmer/Program.cs
+++ ScreenDimmer/Program.cs
@@ -10,8 +10,7 @@ namespace ScreenDimmer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ScreenDimmerContext());
         }
-
-
     }
 }
---
+++ ScreenDimmer/ScreenDimmerContext.cs
@@ -0,0 +1,29 @@
+﻿using ScreenDimmer.Properties;
+using System;
+using System.Windows.Forms;
+
+namespace ScreenDimmer
+{
+    public class ScreenDimmerContext : ApplicationContext
+    {
+        private readonly NotifyIcon trayIcon;
+
+        public ScreenDimmerContext()
+        {
+            trayIcon = new NotifyIcon
+            {
+                Icon = Resources.ScreenDimmerTrayIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
