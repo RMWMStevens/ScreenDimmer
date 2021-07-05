4c37d8ea7d42629a2bde290fc8bf56d427f95bcc
Roel Stevens <rshunter99@gmail.com>
2021-07-05 20:07:06 +0200

    Initial commit

---
+++ ScreenDimmer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ScreenDimmer
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+
+
+    }
+}
