d2bff5918dcae834b9b06b45f8c7ef0152d5db96
Roel Stevens <rshunter99@gmail.com>
2021-07-06 18:06:19 +0200

    Dimming works

---
+++ ScreenDimmer/Contexts/ScreenDimmerContext.cs
@@ -0,0 +1,59 @@
+﻿using ScreenDimmer.Forms;
+using ScreenDimmer.Properties;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ScreenDimmer.Contexts
+{
+    public class ScreenDimmerContext : ApplicationContext
+    {
+        readonly NotifyIcon trayIcon;
+        readonly Screen[] screens;
+        readonly List<DimForm> dimForms;
+
+        public ScreenDimmerContext()
+        {
+            trayIcon = new NotifyIcon
+            {
+                Icon = Resources.ScreenDimmerTrayIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true,
+                Text = "ScreenDimmer - Save your eyes!"
+            };
+
+            screens = Screen.AllScreens;
+            dimForms = new List<DimForm>();
+            EnableDimming();
+        }
+
+        void EnableDimming()
+        {
+            foreach(var screen in screens)
+            {
+                if (screen.Primary) { continue; }
+                var dimForm = new DimForm(screen);
+                dimForm.Show();
+                dimForms.Add(dimForm);
+            }
+        }
+
+        void DisableDimming()
+        {
+            foreach (var dimForm in dimForms)
+            {
+                dimForm.Close();
+            }
+            dimForms.Clear();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            trayIcon.Dispose();
+            Application.Exit();
+        }
+    }
+}
---
+++ ScreenDimmer/Forms/DimForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ScreenDimmer.Forms
+{
+    public partial class DimForm : Form
+    {
+        public DimForm(Screen screen)
+        {
+            InitializeComponent();
+
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(screen.Bounds.X, screen.Bounds.Y);
+            Size = new Size(screen.Bounds.Width, screen.Bounds.Height);
+        }
+    }
+}
