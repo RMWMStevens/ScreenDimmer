b4b4aa7efd0bf8b33ac07f62bcf1102658c1f84f
Roel Stevens <rshunter99@gmail.com>
2021-07-06 22:40:26 +0200

    Fixed comments, gave some credit and fixed ScreenDimmer showing up in Alt-Tab

--- ScreenDimmer/Forms/DimForm.cs
+++ ScreenDimmer/Forms/DimForm.cs
@@ -5,6 +5,16 @@ namespace ScreenDimmer.Forms
 {
     public partial class DimForm : Form
     {
+        protected override CreateParams CreateParams /* Credit: https://github.com/zingmars/ScreenBlank */
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x80;
+                return cp;
+            }
+        }
+
         public DimForm(Screen screen)
         {
             InitializeComponent();
@@ -12,6 +22,7 @@ namespace ScreenDimmer.Forms
             StartPosition = FormStartPosition.Manual;
             Location = new Point(screen.Bounds.X, screen.Bounds.Y);
             Size = new Size(screen.Bounds.Width, screen.Bounds.Height);
+            ShowInTaskbar = false;
         }
     }
 }
--- ScreenDimmer/Services/HotKeyService.cs
+++ ScreenDimmer/Services/HotKeyService.cs
@@ -4,7 +4,10 @@ using System.Windows.Forms;
 
 namespace ScreenDimmer.Services
 {
-    public class HotKeyService : Form /* Credit: https://stackoverflow.com/questions/15413172/capture-a-keyboard-keypress-in-the-background */
+    /* 
+        Credit for capturing key presses: https://stackoverflow.com/questions/15413172/capture-a-keyboard-keypress-in-the-background
+    */
+    public class HotKeyService : Form
     {
         int hotKeyId = 1;
 
@@ -12,9 +15,11 @@ namespace ScreenDimmer.Services
 
         public void Register()
         {
-            // Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
-            // Compute the addition of each combination of the keys you want to be pressed
-            // ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
+            /* 
+                Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+                Compute the addition of each combination of the keys you want to be pressed
+                ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
+            */
             RegisterHotKey(this.Handle, hotKeyId, hotKeyId * 2, (int)Keys.Decimal);
         }
 
