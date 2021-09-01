e55d8f4121c070169e96843c67357387a58e0d8f
Roel Stevens <38633494+RMWMStevens@users.noreply.github.com>
2021-09-01 10:44:42 +0200

    Removed comment, made the key a parameter

--- ScreenDimmer/Services/HotKeyService.cs
+++ ScreenDimmer/Services/HotKeyService.cs
@@ -14,18 +14,13 @@ namespace ScreenDimmer.Services
 
         public event EventHandler HotKeyPressedEvent;
 
-        public void Register()
+        public void Register(Keys keys = Keys.Decimal)
         {
-            /* 
-                Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
-                Compute the addition of each combination of the keys you want to be pressed
-                ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
-            */
             RegisterHotKey(
                 Handle,
                 hotKeyId,
                 hotKeyId * 2,
-                (int)Keys.Decimal);
+                (int)keys);
         }
 
         public void Unregister()
